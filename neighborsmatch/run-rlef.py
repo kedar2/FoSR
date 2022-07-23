2ac9d2c588e075695c3e5eea7233e9c303c451d1
Kedar Karhadkar <kedarkarhadkar@gmail.com>
2022-07-23 14:09:37 -0700

    Add files via upload

--- neighborsmatch/run-rlef.py
+++ neighborsmatch/run-rlef.py
@@ -1,6 +1,8 @@
 from attrdict import AttrDict
 from experiment import Experiment
 import torch
+import random
+from math import inf
 import numpy as np
 import task
 import rewiring
@@ -8,7 +10,9 @@ import rewiring_rlef
 from torch.nn.functional import one_hot
 from torch_geometric.utils import to_networkx, from_networkx
 
-
+torch.manual_seed(123)
+random.seed(123)
+np.random.seed(123)
 
 G = task.path_of_cliques(3, 10)
 vertices_to_label = list(range(0, 9))
@@ -27,15 +31,20 @@ def produce_rewired_dataset(dataset_source, num_iterations):
         dset[i].edge_index = from_networkx(G).edge_index
     return dset
 
-def log_to_file(message, filename="neighborsmatch.txt"):
+def log_to_file(message, filename="neighborsmatch2.txt"):
     print(message)
     file = open(filename, "a")
     file.write(message)
     file.close()
 
-for iteration_count in iteration_counts:
-    nmatch = task.create_neighborsmatch_dataset(G, 29, vertices_to_label, 10000)
-    nmatch = produce_rewired_dataset(nmatch, num_iterations=iteration_count)
+nmatch = task.create_neighborsmatch_dataset(G, 29, vertices_to_label, 10000)
+
+
+for iteration_count in range(10, 160, 10):
+    torch.manual_seed(123)
+    random.seed(123)
+    np.random.seed(123)
+    nmatch = produce_rewired_dataset(nmatch, num_iterations=10)
     hyperparams = {
     "neighborsmatch": AttrDict({"dropout": 0.0, "num_layers": 6, "hidden_dim": 64, "learning_rate": 0.001})
     }
@@ -43,15 +52,15 @@ for iteration_count in iteration_counts:
     num_trials=10
     name = "neighborsmatch"
     accuracies = []
-    print(f"TESTING: {name} (RLEF), ITERATION COUNT: {iteration_count}")
+    print(f"TESTING: {name} (SDRF), ITERATION COUNT: {iteration_count}")
     for trial in range(num_trials):
 
         args = AttrDict({"dataset": nmatch, "layer_type": "GAT", "display": True})
         args += hyperparams["neighborsmatch"]
-        train_acc, validation_acc, test_acc = Experiment(args).run()
+        train_acc = Experiment(args).run()
         accuracies.append(train_acc.item())
         torch.cuda.empty_cache()
-        log_to_file(f"RESULTS FOR {name} (RLEF), ITERATION COUNT: {iteration_count}:\n")
+        log_to_file(f"RESULTS FOR {name} (SDRF), ITERATION COUNT: {iteration_count}:\n")
         log_to_file(f"average acc: {np.mean(accuracies)}\n")
         log_to_file(f"plus/minus:  {2 * np.std(accuracies)/(num_trials ** 0.5)}\n\n")
     
